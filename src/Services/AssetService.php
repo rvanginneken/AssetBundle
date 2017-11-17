e4f7aa60ce8ac4eae6c66a53943a0a2fb68ac839
Roy Van Ginneken <royvanginneken@gmail.com>
2017-11-17 09:28:14 +0100

    more dynamic asset service

--- src/Services/AssetService.php
+++ src/Services/AssetService.php
@@ -1,16 +1,24 @@
 <?php
 
-namespace  RVanGinneken\DynamicAssetIncludeBundle\Services;
+namespace RVanGinneken\DynamicAssetIncludeBundle\Services;
 
 use Symfony\Component\HttpFoundation\RequestStack;
 
 class AssetService
 {
+    public const TARGET_HEAD = 1;
+    public const TARGET_BODY = 2;
+
+    private const RENDER_TYPE_INLINE = 1;
+    private const RENDER_TYPE_SCRIPT = 2;
+    private const RENDER_TYPE_SCRIPT_TO_INLINE = 3;
+
     private $requestStack;
     private $cacheService;
     private $webDir;
 
-    private $assets;
+    private $types;
+    private $assets = [];
 
     public function __construct(RequestStack $requestStack, CacheService $cacheService, string $webDir)
     {
@@ -18,96 +26,71 @@ class AssetService
         $this->cacheService = $cacheService;
         $this->webDir = realpath($webDir);
 
-        $this->assets = [
-            'css_file' => [],
-            'css' => [],
-            'javascript_file' => [],
-            'javascript' => [],
+        $this->types = [
+            'css_file' => ['target' => self::TARGET_HEAD, 'template' => '<style>%s</style>', 'render_type' => self::RENDER_TYPE_SCRIPT_TO_INLINE, 'priority' => 128],
+            'css' => ['target' => self::TARGET_HEAD, 'template' => '%s', 'render_type' => self::RENDER_TYPE_INLINE, 'priority' => 64],
+            'javascript_file' => ['target' => self::TARGET_BODY, 'template' => '<script type="text/javascript" src="%s"></script>', 'render_type' => self::RENDER_TYPE_SCRIPT, 'priority' => 128],
+            'javascript' => ['target' => self::TARGET_BODY, 'template' => '%s', 'render_type' => self::RENDER_TYPE_INLINE, 'priority' => 64],
         ];
     }
 
     public function addAsset(string $type, string $asset, int $priority): void
     {
-        if (!isset($this->assets[$type])) {
+        if (!isset($this->types[$type])) {
             throw new \RuntimeException('Type \''.$type.'\' is not supported by the \''.__CLASS__.'\'.');
         }
 
         $this->assets[$type][] = ['asset' => $asset, 'priority' => $priority];
     }
 
-    public static function compareAssets(array $a, $b): int
+    public function render(int $target): string
+    {
+        $key = $this->getCacheKey('asset_render_'.$target);
+
+        if (null === $html = $this->cacheService->cacheGet($key)) {
+            $html = '';
+
+            uasort($this->types, ['self', 'comparePriorities']);
+            foreach ($this->types as $type => $config) {
+                if ($config['target'] !== $target || !isset($this->assets[$type])) {
+                    continue;
+                }
+
+                usort($this->assets[$type], ['self', 'comparePriorities']);
+                foreach ($this->assets[$type] as $asset) {
+                    switch ($config['render_type']) {
+                        case self::RENDER_TYPE_INLINE:
+                            $html .= sprintf($config['template'], $asset['asset']);
+                            break;
+                        case self::RENDER_TYPE_SCRIPT:
+                            if (0 !== strpos($asset['asset'], 'http')) {
+                                $asset['asset'] = '/'.ltrim($asset['asset'], '/');
+                            }
+                            $html .= sprintf($config['template'], $asset['asset']);
+                            break;
+                        case self::RENDER_TYPE_SCRIPT_TO_INLINE:
+                            if (0 !== strpos($asset['asset'], 'http')) {
+                                $asset['asset'] = $this->webDir.'/'.ltrim($asset['asset'], '/');
+                            }
+                            $html .= sprintf($config['template'], file_get_contents($asset['asset']));
+                            break;
+                    }
+                }
+            }
+
+            $this->cacheService->cacheSet($key, $html);
+        }
+
+        return $html;
+    }
+
+    public static function comparePriorities(array $a, $b): int
     {
         if ($a['priority'] === $b['priority']) {
             return 0;
         }
 
         return ($a['priority'] > $b['priority']) ? -1 : +1;
-    }
-
-    public function renderCss(): string
-    {
-        $key = $this->getCacheKey('asset_render_css');
-
-        if (null === $html = $this->cacheService->cacheGet($key)) {
-            $html = '';
-
-            if (\count($this->assets['css_file'])) {
-                usort($this->assets['css_file'], ['self', 'compareAssets']);
-
-                $html .= '<style>';
-                foreach ($this->assets['css_file'] as $cssFile) {
-                    if (0 !== strpos($cssFile['asset'], 'http')) {
-                        $cssFile['asset'] = $this->webDir.'/'.ltrim($cssFile['asset'], '/');
-                    }
-                    $html .= file_get_contents($cssFile['asset']);
-                }
-                $html .= '</style>';
-            }
-
-            if (\count($this->assets['css'])) {
-                usort($this->assets['css'], ['self', 'compareAssets']);
-
-                foreach ($this->assets['css'] as $css) {
-                    $html .= $css['asset'];
-                }
-            }
-
-            $this->cacheService->cacheSet($key, $html);
-        }
-
-        return $html;
-    }
-
-    public function renderJavascript(): string
-    {
-        $key = $this->getCacheKey('asset_render_javascript');
-
-        if (null === $html = $this->cacheService->cacheGet($key)) {
-            $html = '';
-
-            if (\count($this->assets['javascript_file'])) {
-                usort($this->assets['javascript_file'], ['self', 'compareAssets']);
-
-                foreach ($this->assets['javascript_file'] as $javascriptFile) {
-                    if (0 !== strpos($javascriptFile['asset'], 'http')) {
-                        $javascriptFile['asset'] = '/'.ltrim($javascriptFile['asset'], '/');
-                    }
-                    $html .= '<script type="text/javascript" src="'.$javascriptFile['asset'].'"></script>';
-                }
-            }
-
-            if (\count($this->assets['javascript'])) {
-                usort($this->assets['javascript'], ['self', 'compareAssets']);
-
-                foreach ($this->assets['javascript'] as $javascript) {
-                    $html .= $javascript['asset'];
-                }
-            }
-
-            $this->cacheService->cacheSet($key, $html);
-        }
-
-        return $html;
     }
 
     private function getCacheKey(string $prefix): string
