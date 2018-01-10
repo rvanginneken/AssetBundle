062da87cd61040ef27d77555a6f4016650f6788b
Roy Van Ginneken <roy.vanginneken@intracto.com>
2018-01-10 19:02:35 +0100

    Caching for SF prior to 3.1

--- src/Services/CacheService.php
+++ src/Services/CacheService.php
@@ -2,26 +2,42 @@
 
 namespace RVanGinneken\AssetBundle\Services;
 
-use Psr\Cache\CacheItemPoolInterface;
-
 class CacheService
 {
     private $debug;
-    private $cacheItemPool;
 
-    public function __construct(bool $debug, CacheItemPoolInterface $cacheItemPool)
+    private $cacheFile;
+    private $cache = [];
+
+    public function __construct(bool $debug, string $cacheDir)
     {
         $this->debug = $debug;
-        $this->cacheItemPool = $cacheItemPool;
+        $this->cacheFile = $cacheDir.'/rvanginneken/asset/cache.txt';
+
+        if (file_exists($this->cacheFile)) {
+            $this->cache = json_decode(file_get_contents($this->cacheFile), true, 2);
+        }
+    }
+
+    public function __destruct()
+    {
+        if (
+            !file_exists(\dirname($this->cacheFile)) &&
+            !mkdir(\dirname($this->cacheFile), 0777, true) &&
+            !is_dir(\dirname($this->cacheFile))
+        ) {
+            throw new \RuntimeException(sprintf('Directory "%s" was not created', \dirname($this->cacheFile)));
+        }
+        file_put_contents($this->cacheFile, json_encode($this->cache, 0, 2));
     }
 
     public function get(string $key)
     {
-        if ($this->debug || !$this->cacheItemPool->hasItem($key)) {
+        if ($this->debug) {
             return null;
         }
 
-        return $this->cacheItemPool->getItem($key)->get();
+        return $this->cache[$key] ?? null;
     }
 
     public function set(string $key, $value): void
@@ -30,6 +46,6 @@ class CacheService
             return;
         }
 
-        $this->cacheItemPool->save($this->cacheItemPool->getItem($key)->set($value));
+        $this->cache[$key] = $value;
     }
 }
